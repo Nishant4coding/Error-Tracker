99b0ed28f9a987acc84194935ed1a3e375ea64f3
Nishant4coding <srivastava4nishant@gmail.com>
2023-05-28 00:55:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactDiffViewer  from 'react-diff-viewer';
+import './App.css'
 
-function App() {
+const CodeComparison = () => {
+  const [oldCode, setOldCode] = useState('');
+  const [newCode, setNewCode] = useState('');
+
+  const handleOldCodeChange = (event) => {
+    setOldCode(event.target.value);
+  };
+
+  const handleNewCodeChange = (event) => {
+    setNewCode(event.target.value);
+  };
+
+  if(oldCode==='' && newCode==='' ){
+    var value='Enter your code to compare'
+  }
+  else if(oldCode===newCode){
+     value='No Error found'
+  }
+  else if(oldCode!==newCode){
+    value='Errors!'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Find Your Errors!</h1>
+      <div className='content'>
+      <div className='text-area'>
+        <h2>Old Code</h2>
+        <textarea value={oldCode} onChange={handleOldCodeChange} />
+      </div>
+      <div className='text-area'>
+        <h2>New Code</h2>
+        <textarea value={newCode} onChange={handleNewCodeChange} />
+      </div>
+      </div>
+      <div className='Errors'>
+        <h2>{value}</h2>
+        <ReactDiffViewer className='result' oldValue={oldCode} newValue={newCode} splitView={true} />
+      </div>
+      <footer>
+        <p>All right reserved @ Nishant Srivastava</p>
+      </footer>
     </div>
   );
-}
+};
 
-export default App;
+export default CodeComparison;
